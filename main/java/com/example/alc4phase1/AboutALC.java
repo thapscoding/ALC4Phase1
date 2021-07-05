971b961911f9044d112fe6652b108f00d9334130
Coach TP <39503627+thapscoding@users.noreply.github.com>
2021-07-06 01:14:28 +0200

    Update AboutALC.java

--- main/java/com/example/alc4phase1/AboutALC.java
+++ main/java/com/example/alc4phase1/AboutALC.java
@@ -1,3 +1,4 @@
+// In order to successfully use the web view you need permission in the Android manifest file
 package com.example.alc4phase1;
 
 import android.net.http.SslError;
@@ -20,6 +21,7 @@ public class AboutALC extends AppCompatActivity {
         //Load the URL
         myWebView.loadUrl("https://andela.com/alc/");
         // call the setWebViewClient on myWebView
+        // it will onReceivedSslError method
         myWebView.setWebViewClient(new WebViewClient() {
             @Override
             public void onReceivedSslError(final WebView view, final SslErrorHandler handler, final SslError error) {
